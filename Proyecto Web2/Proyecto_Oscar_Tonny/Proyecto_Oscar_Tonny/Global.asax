76e99fcc77bdbaa27e9e1b42719979a1c374f039
HOZCAR <oscarfonseca16@hotmail.com>
2015-06-17 18:17:11 -0600

    creación del proyecto web 2

---
+++ Proyecto Web2/Proyecto_Oscar_Tonny/Proyecto_Oscar_Tonny/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Proyecto_Oscar_Tonny.MvcApplication" Language="C#" %>
---
+++ Proyecto Web2/Proyecto_Oscar_Tonny/Proyecto_Oscar_Tonny/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Proyecto_Oscar_Tonny.Startup))]
+namespace Proyecto_Oscar_Tonny
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
